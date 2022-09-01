a01373c4e295939355abe283240f084960455b66
nandoti <nando.frontend@gmail.com>
2022-09-01 11:47:23 -0300

    criado componente About

---
+++ src/pages/About/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="about">
+      <h2>
+        {" "}
+        Projeto criado para o estudo dirigido da Estácio, utilizando React com
+        roteamento Firebase.
+      </h2>
+    </div>
+  );
+};
+
+export default About;
